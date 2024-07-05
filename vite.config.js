7f8026f365dc752e29392b0985c318bcb894a0c3
Ase444ka <istomina.asia@yandex.ru>
2024-07-05 15:43:26 +0300

    add postcss-nesting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import postcssNesting from 'postcss-nested'
 
 export default defineConfig({
     server: {
       port: 3000
     }, 
+    css: {
+      postcss: {
+        plugins: [
+          postcssNesting
+        ]
+      }
+    }
   }) 
