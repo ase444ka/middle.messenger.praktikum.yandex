e2ac7a2d7ad011863c4c18666cd3585096a7c1da
Ase444ka <istomina.asia@yandex.ru>
2024-07-05 19:20:00 +0300

    add card_partial

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,13 +3,23 @@ import { defineConfig } from 'vite'
 import postcssNesting from 'postcss-nested'
 import handlebars from 'vite-plugin-handlebars';
 
+const pageData = {
+  '/pages/signin_page/index.html': {
+    title: 'Авторизация',
+    fields: [
+      {name: 'имя', class: ''},
+      {name: 'фамилия', class: 'input_simple'},
+  ]
+  },
+};
+
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   plugins: [handlebars({
     partialDirectory: resolve(__dirname, 'src/partials'),
-    context: {
-      title: 'hello yandex'
-    }
+    context(pagePath) {
+      return pageData[pagePath];
+    },
   })],
   server: {
     port: 3000
