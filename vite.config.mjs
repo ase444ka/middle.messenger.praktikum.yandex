66f8df55b234d6cb25ef51a95280a126496b0cea
Ase444ka <istomina.asia@yandex.ru>
2024-07-06 23:59:10 +0300

    add netlify.toml

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,20 +1,17 @@
-import {resolve} from 'path'
-import { defineConfig } from 'vite'
-import postcssNesting from 'postcss-nested'
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
+import postcssNesting from 'postcss-nested';
 import handlebars from 'vite-plugin-handlebars';
-import user from './src/data/user.js'
+import user from './src/data/user.js';
 
 const pageData = {
   '/pages/signin_page/index.html': {
     title: 'Вход',
-    fields: [
-      {name: 'имя'},
-      {name: 'пароль'},
-  ],
-  buttons: [
-    {title: 'авторизация', var: 'primary', action: 'alert("trololo")'},
-    {title: 'нет аккаунта?', var: 'secondary', action: 'alert("trololo")'},
-  ]
+    fields: [{name: 'имя'}, {name: 'пароль'}],
+    buttons: [
+      {title: 'авторизация', var: 'primary', action: 'alert("trololo")'},
+      {title: 'нет аккаунта?', var: 'secondary', action: 'alert("trololo")'},
+    ],
   },
   '/pages/signup_page/index.html': {
     title: 'Регистрация',
@@ -26,11 +23,11 @@ const pageData = {
       {name: 'телефон'},
       {name: 'пароль'},
       {name: 'пароль (ещё раз)', type: 'password'},
-  ],
-  buttons: [
-    {title: 'зарегистрироваться', var: 'primary', action: 'alert("trololo")'},
-    {title: 'войти', var: 'secondary', action: 'alert("trololo")'},
-  ]
+    ],
+    buttons: [
+      {title: 'зарегистрироваться', var: 'primary', action: 'alert("trololo")'},
+      {title: 'войти', var: 'secondary', action: 'alert("trololo")'},
+    ],
   },
   '/pages/user_page/index.html': {
     readonly: true,
@@ -43,31 +40,34 @@ const pageData = {
       {name: 'Фамилия', value: user.surname},
       {name: 'Имя в чате', value: user.nik},
       {name: 'Телефон', value: user.phone},
-  ],
-  buttons: [
-    {title: 'Изменить данные', var: 'primary', action: 'alert("trololo")'},
-    {title: 'Изменить пароль', var: 'primary', action: 'alert("trololo")'},
-    {title: 'Выйти', var: 'danger', action: 'alert("trololo")'},
-  ]
+    ],
+    buttons: [
+      {title: 'Изменить данные', var: 'primary', action: 'alert("trololo")'},
+      {title: 'Изменить пароль', var: 'primary', action: 'alert("trololo")'},
+      {title: 'Выйти', var: 'danger', action: 'alert("trololo")'},
+    ],
   },
 };
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
-  plugins: [handlebars({
-    partialDirectory: resolve(__dirname, 'src/partials'),
-    context(pagePath) {
-      return pageData[pagePath];
-    },
-  })],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+      context(pagePath) {
+        return pageData[pagePath];
+      },
+    }),
+  ],
   server: {
-    port: 3000
-  }, 
+    port: 3000,
+  },
   css: {
     postcss: {
-      plugins: [
-        postcssNesting
-      ]
-    }
-  }
-}) 
+      plugins: [postcssNesting],
+    },
+  },
+});
