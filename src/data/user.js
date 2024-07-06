2d25d308f54a162d687fdce80efa33852e65b438
Ase444ka <istomina.asia@yandex.ru>
2024-07-07 00:13:21 +0300

    deploy-try-2

--- src/data/user.js
+++ src/data/user.js
@@ -1,4 +1,4 @@
-export default {
+const user = {
     name: 'Ася', 
     surname: 'Истомина',
     email: 'istomina.asia@yandex.ru',
@@ -6,3 +6,47 @@ export default {
     nik: 'Ася',
     phone: '+7-981-911-58-79',
 }
+export default {
+    '/pages/signin_page/index.html': {
+      title: 'Вход',
+      fields: [{name: 'имя'}, {name: 'пароль'}],
+      buttons: [
+        {title: 'авторизация', var: 'primary', action: 'alert("trololo")'},
+        {title: 'нет аккаунта?', var: 'secondary', action: 'alert("trololo")'},
+      ],
+    },
+    '/pages/signup_page/index.html': {
+      title: 'Регистрация',
+      fields: [
+        {name: 'почта'},
+        {name: 'логин'},
+        {name: 'имя'},
+        {name: 'фамилия'},
+        {name: 'телефон'},
+        {name: 'пароль'},
+        {name: 'пароль (ещё раз)', type: 'password'},
+      ],
+      buttons: [
+        {title: 'зарегистрироваться', var: 'primary', action: 'alert("trololo")'},
+        {title: 'войти', var: 'secondary', action: 'alert("trololo")'},
+      ],
+    },
+    '/pages/user_page/index.html': {
+      readonly: true,
+      cursor: 'default',
+      name: user.name,
+      fields: [
+        {name: 'Почта', value: user.email},
+        {name: 'Логин', value: user.username},
+        {name: 'Имя', value: user.name},
+        {name: 'Фамилия', value: user.surname},
+        {name: 'Имя в чате', value: user.nik},
+        {name: 'Телефон', value: user.phone},
+      ],
+      buttons: [
+        {title: 'Изменить данные', var: 'primary', action: 'alert("trololo")'},
+        {title: 'Изменить пароль', var: 'primary', action: 'alert("trololo")'},
+        {title: 'Выйти', var: 'danger', action: 'alert("trololo")'},
+      ],
+    },
+  };
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,57 +2,20 @@ import {resolve} from 'path';
 import {defineConfig} from 'vite';
 import postcssNesting from 'postcss-nested';
 import handlebars from 'vite-plugin-handlebars';
-import user from './src/data/user.js';
+import pageData from './src/data/user.js';
 
-const pageData = {
-  '/pages/signin_page/index.html': {
-    title: 'Вход',
-    fields: [{name: 'имя'}, {name: 'пароль'}],
-    buttons: [
-      {title: 'авторизация', var: 'primary', action: 'alert("trololo")'},
-      {title: 'нет аккаунта?', var: 'secondary', action: 'alert("trololo")'},
-    ],
-  },
-  '/pages/signup_page/index.html': {
-    title: 'Регистрация',
-    fields: [
-      {name: 'почта'},
-      {name: 'логин'},
-      {name: 'имя'},
-      {name: 'фамилия'},
-      {name: 'телефон'},
-      {name: 'пароль'},
-      {name: 'пароль (ещё раз)', type: 'password'},
-    ],
-    buttons: [
-      {title: 'зарегистрироваться', var: 'primary', action: 'alert("trololo")'},
-      {title: 'войти', var: 'secondary', action: 'alert("trololo")'},
-    ],
-  },
-  '/pages/user_page/index.html': {
-    readonly: true,
-    cursor: 'default',
-    name: user.name,
-    fields: [
-      {name: 'Почта', value: user.email},
-      {name: 'Логин', value: user.username},
-      {name: 'Имя', value: user.name},
-      {name: 'Фамилия', value: user.surname},
-      {name: 'Имя в чате', value: user.nik},
-      {name: 'Телефон', value: user.phone},
-    ],
-    buttons: [
-      {title: 'Изменить данные', var: 'primary', action: 'alert("trololo")'},
-      {title: 'Изменить пароль', var: 'primary', action: 'alert("trololo")'},
-      {title: 'Выйти', var: 'danger', action: 'alert("trololo")'},
-    ],
-  },
-};
+
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        user: resolve(__dirname, 'src/pages/user_page/index.html'),
+      },
+    },
   },
   plugins: [
     handlebars({
